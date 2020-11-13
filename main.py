a9ec4f8796dbf28c56794bc38956bbce05b5b269
nerstak <roy.karst@gmail.com>
2020-11-13 20:35:54 +0100

    Adding new file

---
+++ main.py
@@ -0,0 +1,17 @@
+def addition(a,b):
+    pass
+
+def substraction(a,b):
+    pass
+
+def multiplication(a,b):
+    pass
+    
+def division(a,b):
+    pass
+
+
+print("63 + 9 = " + str(addition(63,9)))
+print("42 - 8 = " + str(substraction(42,8)))
+print("8 * 6 = " + str(multiplication(8,6)))
+print("12 / 4 = " + str(division(12,4)))
