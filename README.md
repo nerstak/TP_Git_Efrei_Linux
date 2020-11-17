584cab88923b64ea19d6309eb017ff4594bd7452
nerstak <roy.karst@gmail.com>
2020-11-17 16:38:27 +0100

    Added comments & ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # TP_Git_Efrei_Linux
+
+Ce dépôt contient un projet de support pour travailler sur le TP de [Git](https://hackmd.io/2jPGKQhRRJOYG5a-q6CKog?view).
--- main.py
+++ main.py
@@ -1,16 +1,31 @@
+"""
+À compléter pour faire la somme de a et b
+"""
 def addition(a,b):
     pass
 
+"""
+À compléter pour faire la soustraction de a par b
+"""
 def substraction(a,b):
     pass
 
+"""
+À compléter pour faire la multiplication de a et b
+"""
 def multiplication(a,b):
     pass
     
+"""
+À compléter pour faire la division de a par b
+"""
 def division(a,b):
     pass
 
 
+"""
+Tests des fonctions
+"""
 print("63 + 9 = " + str(addition(63,9)))
 print("42 - 8 = " + str(substraction(42,8)))
 print("8 * 6 = " + str(multiplication(8,6)))
