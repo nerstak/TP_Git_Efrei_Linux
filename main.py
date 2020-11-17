91bfab83efd123f8dbdc2d7696ee3919ab542db3
nerstak <>
2020-11-17 16:41:23 +0100

    More comments

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 """
-À compléter pour faire la somme de a et b
+À compléter pour faire la somme de a et b et retourner le résultat
 """
 def addition(a,b):
     pass
 
 """
-À compléter pour faire la soustraction de a par b
+À compléter pour faire la soustraction de a par b et retourner le résultat
 """
 def substraction(a,b):
     pass
 
 """
-À compléter pour faire la multiplication de a et b
+À compléter pour faire la multiplication de a et b et retourner le résultat
 """
 def multiplication(a,b):
     pass
     
 """
-À compléter pour faire la division de a par b
+À compléter pour faire la division de a par b et retourner le résultat
 """
 def division(a,b):
     pass
